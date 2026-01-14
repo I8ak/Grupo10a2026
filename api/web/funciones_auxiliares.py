a507e92c5a034721d04b77412f545ea548a091f9
I8ak <alhelimiishak@gmail.com>
2026-01-14 10:28:10 +0100

    fotos

--- api/web/funciones_auxiliares.py
+++ api/web/funciones_auxiliares.py
@@ -4,5 +4,9 @@ import json
 class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal): return float(obj)
+        
+    def calculariva(importe):
+
+                 return importe*0.21
 
 
