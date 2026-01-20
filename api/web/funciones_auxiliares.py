efe068b83e47df0bbe8720ccbc9b3939e52f117c
I8ak <alhelimiishak@gmail.com>
2026-01-20 09:47:33 +0100

    mas coasas nuevas

--- api/web/funciones_auxiliares.py
+++ api/web/funciones_auxiliares.py
@@ -5,5 +5,5 @@ class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal): return float(obj)
         
-    def calculariva(importe):
-        return importe*0.21
+    def calcularIVA(importe):
+        return importe * 0.21
--- api/web/rutas_perfumes.py
+++ api/web/rutas_perfumes.py
@@ -1,6 +1,6 @@
 from flask import request, Blueprint, jsonify
 import controlador_perfumes
-from funciones_auxiliares import Encoder
+from funciones_auxiliares import Encoder, calcularIVA
 
 bp = Blueprint('perfumes', __name__)
 
@@ -21,10 +21,10 @@ def guardar_perfume():
         perfume_json = request.json
         nombre = perfume_json["nombre"]
         descripcion = perfume_json["descripcion"]
-        precio=perfume_json["precio"]
-        foto=perfume_json["foto"]
-        notas=perfume_json["notas"]
-        respuesta,code=controlador_perfumes.insertar_perfume(nombre, descripcion,precio,foto,notas)
+        precio = calcularIVA(perfume_json["precio"])
+        foto = perfume_json["foto"]
+        notas = perfume_json["notas"]
+        respuesta, code = controlador_perfumes.insertar_perfume(nombre, descripcion, precio, foto, notas)
     else:
         respuesta={"status":"Bad request"}
         code=401
