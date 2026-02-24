ce352c97b8a008d47b91fadce224d0270dff6ec7
SantiagoRepos <daniel.santiago12@educa.madrid.org>
2026-02-24 09:43:07 +0100

    Dast2

---
+++ tests/dast/prueba_dast.py
@@ -0,0 +1,23 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+BASE_URL = os.getenv("10.227.87.30:60010")
+
+def test_xss_login():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(f"{BASE_URL}/login")
+
+    username = driver.find_element(By.NAME, "username")
+    username.send_keys("<script>alert(1)</script>")
+
+    driver.find_element(By.NAME, "submit").click()
+
+    assert "<script>" not in driver.page_source
+
+    driver.quit()
