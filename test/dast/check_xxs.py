7d9eadd49db5566c96618f781da087c3c8b8d469
I8ak <alhelimiishak@gmail.com>
2026-02-25 09:54:16 +0100

    cosas nuevs

--- test/dast/check_xxs.py
+++ test/dast/check_xxs.py
@@ -8,7 +8,7 @@ def test_xss_login():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
 
     driver.get(f"{BASE_URL}/login")
