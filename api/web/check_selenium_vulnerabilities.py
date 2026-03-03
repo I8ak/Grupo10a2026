300bf7326b02e54883b949451786977cee196ac2
I8ak <alhelimiishak@gmail.com>
2026-03-03 09:09:06 +0100

    intentar arreglarlo

--- api/web/check_selenium_vulnerabilities.py
+++ api/web/check_selenium_vulnerabilities.py
@@ -8,7 +8,7 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument('--disable-gpu')
-
+options.binary_location = "/usr/bin/chromium"
 
 # En Linux/Docker usamos el binario de chromium
 service = Service('/usr/bin/chromedriver')
